502dd51590798fbfa9b065aaab0702ebd6f78d8d
naresh krishnamoorthy <naresh.ideas2it@gmail.com>
2022-09-25 18:54:32 +0530

    adding react components from example

---
+++ src/WebBuilder.js
@@ -0,0 +1,74 @@
+import { useEffect } from "react";
+import grapesjs from 'grapesjs'
+import 'grapesjs/dist/css/grapes.min.css'
+import 'grapesjs/dist/grapes.min.js'
+import plugin from 'grapesjs-preset-webpage'
+import ReactText from "./simple-react-text";
+import BaseReactComponent from './base-react-component';
+import ReactComponents from './react-components';
+import MuiComponents from './epos';
+
+function WebBuilder() {
+  useEffect(() => {
+  const editor =  grapesjs.init({
+      container: '#gjs',
+      height: '700px',
+      width: '100%',
+      plugins: [plugin, BaseReactComponent, ReactComponents, MuiComponents],
+      storageManager: {
+        id: 'gjs-',
+        type: 'local',
+        autosave: true,
+        storeComponents: true,
+        storeStyles: true,
+        storeHtml: true,
+        storeCss: true,
+      },
+      deviceManager: {
+        devices:
+        [
+          {
+            id: 'desktop',
+            name: 'Desktop',
+            width: '',
+          },
+          {
+            id: 'tablet',
+            name: 'Tablet',
+            width: '768px',
+            widthMedia: '992px',
+          },
+          {
+            id: 'mobilePortrait',
+            name: 'Mobile portrait',
+            width: '320px',
+            widthMedia: '575px',
+          },
+        ]
+      },
+      pluginsOpts: {
+        'grapesjs-preset-webpage': {
+          blocks: ['link-block', 'quote', 'text-basic'],
+        },
+      }
+    })
+
+    editor.BlockManager.add('my-block-id', {
+      label: 'ReactText',
+      category: 'ePOS',
+      components: ReactText,
+      activate: true
+      // ...
+    })
+
+  },[])
+
+ return (
+  
+   <div id="gjs">
+     TESTS
+   </div>
+  
+ );
+}
+export default WebBuilder;
---
+++ src/simple-react-text.js
@@ -0,0 +1,8 @@
+function ReactText() {
+    return (
+      <div>
+        Integration with React Components
+      </div>
+    );
+   };
+   export default ReactText;
